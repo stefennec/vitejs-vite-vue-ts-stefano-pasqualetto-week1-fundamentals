ad3f6b180be38c39520c815186c92746dd7ff28d
stefennec <32901336+stefennec@users.noreply.github.com>
2022-10-06 19:04:02 +0200

    4 files modified

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,26 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue';
-</script>
+<template>
+  <div class="container" style="max-width: 440">
+    <div class="card">
+    <img src="https://www.w3schools.com/howto/img_avatar2.png" className="card-img-top" alt="..." />
+        <div className="card-header">PRODUCT NAME</div>
+        <div className="card-body">
+          <h5 className="card-title">COLOR: <span className="color-box" /></h5>
+          <h5 className="card-title">GENDER: Female</h5>
+          <h5 className="card-title">PRICE: € 000</h5>
+          <h5 className="card-title">
+            <span>SIZE: </span>
+            <span className="sizes">
+              <span>M</span>-
+              <span className="size-available">L</span>-
+              <span>XL</span>
+            </span>
+          </h5>
+          <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, asperiores, rem. </p>
+          <a href="#" className="btn btn-primary">Visit WebSite</a>
+          </div>
+  </div>
+</template>
 
-<template></template>
+<script setup lang="ts"></script>
 
 <style scoped></style>
